18cb8a573a8e9a208d0dc52141c960bd2235d70b
unknown <mhmmdazmye@gmail.com>
2023-03-29 05:40:20 +0700

    add data to localstorage

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -1,64 +1,78 @@
+import { useState } from "react";
+
 const Register = ({ className, toLogin }) => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [fullName, setFullName] = useState('')
+  const [gender, setGender] = useState('')
+  const [address, setAddress] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault()
+    const userData = {
+      email, password, fullName, gender, address, phoneNumber
+    }
+
+    console.log(userData)
+    localStorage.setItem('User', JSON.stringify(userData))
+  }
   return (
     <div
       className={`absolute mt-10 rounded-md  bg-black/80 w-72 text-white p-5 ${className}`}
     >
       <h2 className="font-semibold text-lg mb-4">REGISTER</h2>
-      <form>
+      <form onSubmit={handleOnSubmit}>
         <input
           type="email"
-          name=""
-          id=""
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           className="w-full mb-3 rounded-md p-2 placeholder-white border-2 border-white bg-zinc-500 focus:outline-none"
           placeholder="Email"
         />
 
         <input
           type="password"
-          name=""
-          id=""
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
           className="w-full mb-3 rounded-md p-2 placeholder-white border-2 border-white bg-zinc-500 focus:outline-none"
           placeholder="Password"
         />
 
         <input
           type="text"
-          name=""
-          id=""
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
           className="w-full mb-3 rounded-md p-2 placeholder-white border-2 border-white bg-zinc-500 focus:outline-none"
           placeholder="FullName"
         />
 
         <select
-          name=""
-          id=""
+        onChange={(e) => setGender(e.target.value)}
+        value={'Gender'}
           className="w-full mb-3 rounded-md p-1 placeholder-white border-2 border-white bg-zinc-500 focus:outline-none"
         >
-          <option value="Gender" selected className="hidden">
-            Gender
-          </option>
           <option value="Male">Male</option>
           <option value="Female">Female</option>
         </select>
 
         <input
           type="text"
-          name=""
-          id=""
+          value={phoneNumber}
+          onChange={(e) => setPhoneNumber(e.target.value)}
           className="w-full mb-3 rounded-md p-2 placeholder-white border-2 border-white bg-zinc-500 focus:outline-none"
           placeholder="Phone"
         />
 
         <textarea
-          name=""
-          id=""
           placeholder="Address"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
           className="w-full mb-3 rounded-md p-2 placeholder-white border-2 border-white bg-zinc-500 focus:outline-none"
         ></textarea>
 
         <button
           className="w-full bg-white py-2 rounded-md mb-2 text-red-700 font-semibold"
-          type="button"
         >
           Register
         </button>
